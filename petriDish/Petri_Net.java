e3961a998dc9f1516324d5000e53c884ab446050
Susanne <suzze-t@hotmail.com>
2017-03-02 14:45:49 +0100

    :)

--- petriDish/Petri_Net.java
+++ petriDish/Petri_Net.java
@@ -1,7 +1,19 @@
-
+import java.awt.Point;
+ 
 public class Petri_Net {
 	Place start;
 	Place end;
-	List<Transition>
-	List<Place>
+	List<Transition> = new ArrayList<Transition>();
+	List<Place> = new ArrayList<Place>();
+
+	/*
+	public Point get_current_position(){		
+	}
+	*/
+	
+	public void place_to_transition(Place P, Transition T){		
+	}
+	public void transition_to_place(Transition T, Place P){
+	}
+
 }
