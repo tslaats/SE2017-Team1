7ece72cdd66fa26d491972a9c5aeedad56961044
Lasse HH <ninjalf2@gmail.com>
2017-03-16 10:57:23 +0100

    Added ConresGraph interfaces
    And removed old interface files

---
+++ CRSemantics/CRSemantics/src/interfaces/ConresActivity.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+public interface ConresActivity {
+    public int id;
+    public Point position;
+    public String name;
+    public String role;
+    public bool isExecuted;
+    public bool isPending;
+
+    public ConresActivity(int id, Point position, String name, String role);
+
+    public string toString();
+}
