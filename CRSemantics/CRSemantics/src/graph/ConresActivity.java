4a588bac8d0328b786cad34ceac8567962a66d5c
Josephine Rehak <justallphine@googlemail.com>
2017-03-23 11:46:02 +0100

    added nested Graph

--- CRSemantics/CRSemantics/src/graph/ConresActivity.java
+++ CRSemantics/CRSemantics/src/graph/ConresActivity.java
@@ -1,5 +1,7 @@
 package graph;
 
+import interfaces.Graph;
+
 public class ConresActivity {
     public int id;
     public Point position;
@@ -7,17 +9,19 @@ public class ConresActivity {
     public String role;
     public boolean isExecuted;
     public boolean isPending;
+    public Graph nestedGraph;
 
-    public ConresActivity(int id, Point position, String name, String role, boolean isPending){
+    public ConresActivity(int id, Point position, String name, String role, boolean isPending, Graph nestedGraph){
         this.id = id;
         this.position = position;
         this.name = name;
         this.role = role;
         this.isPending = isPending;
+        this.nestedGraph = nestedGraph;
         this.isExecuted = false;
     }
 
     public String toString(){
-
+        return " ";
     }
 }
