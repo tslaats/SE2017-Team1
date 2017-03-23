5e1ebbb2b87202a115467d927b63750edd497219
Josephine Rehak <justallphine@googlemail.com>
2017-03-23 11:22:58 +0100

    added constructor for CRGraph

--- CRSemantics/CRSemantics/src/graph/ConresGraph.java
+++ CRSemantics/CRSemantics/src/graph/ConresGraph.java
@@ -7,4 +7,10 @@ import interfaces.Graph;
 public class ConresGraph implements Graph {
     public ArrayList<ConresActivity> activities;
     public ArrayList<ConresRelation> relations;
+
+    public ConresGraph(ArrayList<ConresActivity> activities, ArrayList<ConresRelation> relations){
+        this.activities = activities;
+        this.relations = relations;
+
+    }
 }
