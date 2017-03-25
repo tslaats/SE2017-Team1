49fd1383a7e23d93526588691d30454eb31d845c
Lasse HH <ninjalf2@gmail.com>
2017-03-26 00:57:15 +0100

    Remove some weird unnecessary whitespace

--- CRSemantics/CRSemantics/src/interfaces/Semantics.java
+++ CRSemantics/CRSemantics/src/interfaces/Semantics.java
@@ -3,67 +3,11 @@ package interfaces;
 import java.util.List;
 
 public interface Semantics {
-	
-	public List<Integer> getPossibleActions(Graph graph) throws Exception;
 
-	public Graph executeAction(Graph graph, List<Integer> ids) throws Exception;
-	
-	public boolean isFinished(Graph graph) throws Exception;
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+    public List<Integer> getPossibleActions(Graph graph) throws Exception;
+
+    public Graph executeAction(Graph graph, List<Integer> ids) throws Exception;
+
+    public boolean isFinished(Graph graph) throws Exception;
+
 }
